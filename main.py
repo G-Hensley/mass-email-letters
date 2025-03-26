b02c5df60ff655072d31cd0b71e59fbfdaaa0666
Gavin Hensley <gavinhensley@protonmail.com>
2025-03-26 00:07:47 -0400

    Update main.py with comments

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Opens the starting letter template
 with open("./Input/Letters/starting_letter.txt") as note:
     contents = note.read()
 
+# Opens the list of names to be invited
 with open("./Input/Names/invited_names.txt") as names:
     letter_names = names.readlines()
 
+# Creates a list of names without the new line characters
 updated_names = []
 for string in letter_names:
     the_name = string.strip()
     updated_names.append(the_name)
 
+# Creates a new file for each name in the list
 for name in updated_names:
     new_file = f"./Output/ReadyToSend/letter_for_{name}.txt"
     with open(new_file, mode='w') as new_letter:
